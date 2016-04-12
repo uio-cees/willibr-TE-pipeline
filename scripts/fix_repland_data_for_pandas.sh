bec15c6ad8a42f4903895d15378e9bece061f448
willibry <williambrynildsen@gmail.com>
2016-04-12 13:14:32 +0200

    Added files via upload

---
+++ scripts/fix_repland_data_for_pandas.sh
@@ -0,0 +1,56 @@
+#	Purpose: trim parseRM_GetLandscape.pl output for import to pandas
+
+
+FILEPATH=$1
+
+cd $FILEPATH
+
+#----------------------------------START OF LOOP-----------------------------------------#
+
+for i in $(ls $FILEPATH );
+do
+
+
+#	Need total number of lines to "tail out" the correct amount of lines
+LENGTH=$(cat $i | wc -l)
+TAIL=$(($LENGTH-3))
+
+#	This is the body of the data
+column -t $i | tail -n $TAIL > $i.repland
+
+#	Getting header, removing "[" and "]"
+head -n 2  $i | tail -n1 | \
+sed 's/\[//g' | sed 's/\]//g' | column -t \
+	>  header
+
+#	Getting length of repland_complete
+REPLAND_LENGTH=$(cat $i.repland | wc -l)
+
+#	Attempting to create column file for later merge
+yes ${i%%.*} | head -n $REPLAND_LENGTH > ${i%%.*}.left
+
+#	Pasting left alias column with the correct data
+paste ${i%%.*}.left $i.repland > ${i%%.*}.RL.tab
+
+
+#	Deleting files
+rm  $i.repland ${i%%.*}.left
+
+done
+#---------------------------------END OF LOOP--------------------------------------------#
+
+echo "ALIAS" $(cat header) > header_new
+
+cat *RL.tab > teleost.RepeatLandscapes.body
+
+awk '{$NF=""; print $0}' teleost.RepeatLandscapes.body \
+	> teleost.body
+
+cat header_new teleost.body > teleost.RepeatLandscapes.csv
+
+rm teleost.RepeatLandscapes.tab
+rm *RL.tab
+rm header
+rm header_new
+rm teleost.body
+rm teleost.RepeatLandscapes.body
